64b934761bad433bf678867130362b00fd95a514
SunHowe <tosunhowe@gmail.com>
2024-04-13 18:02:10 +0800

    屏幕适配组件

---
+++ Unity/Assets/Scripts/ModelView/Client/Module/FUI/FUIScreenAdaptorComponent.cs
@@ -0,0 +1,32 @@
+﻿using FairyGUI;
+using UnityEngine;
+
+namespace ET.Client
+{
+    /// <summary>
+    /// FUI 屏幕适配组件
+    /// </summary>
+    [ComponentOf(typeof(Scene))]
+    public sealed class FUIScreenAdaptorComponent : Entity, IAwake<UIContentScaler.ScreenMatchMode, int, int>
+    {
+        public UIContentScaler.ScreenMatchMode ScreenMatchMode { get; set; }
+        public int DesignResolutionX { get; set; }
+        public int DesignResolutionY { get; set; }
+        
+        /// <summary>
+        /// UI 安全区域。(逻辑尺寸)
+        /// </summary>
+        public Rect UISafeArea { get; set; }
+    }
+    
+    /// <summary>
+    /// UI 安全区域更新事件
+    /// </summary>
+    public struct FUIScreenAdaptorChangedEvent
+    {
+        /// <summary>
+        /// UI 安全区域。(逻辑尺寸)
+        /// </summary>
+        public Rect UISafeArea { get; set; }
+    }
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/Module/ScreenAdaptor/ScreenAdaptorComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    /// <summary>
+    /// 屏幕适配组件
+    /// </summary>
+    [ComponentOf(typeof(Scene))]
+    public sealed class ScreenAdaptorComponent : Entity, IAwake
+    {
+        /// <summary>
+        /// 屏幕安全区域
+        /// </summary>
+        public Rect SafeArea { get; set; }
+    }
+    
+    /// <summary>
+    /// 屏幕安全区域更新事件
+    /// </summary>
+    public struct ScreenSafeAreaUpdatedEvent
+    {
+        public Rect SafeArea;
+    }
+}

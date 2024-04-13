35d05d98ca663cf952cd71581655a75ab490c4f8
SunHowe <tosunhowe@gmail.com>
2024-04-13 15:30:05 +0800

    [feat] FUIHelper

---
+++ Unity/Assets/Scripts/HotfixView/Client/Module/FUI/FUIHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace ET.Client
+{
+    public static class FUIHelper
+    {
+        [EnableAccessEntiyChild]
+        public static async ETTask<FUI> OpenUIAsync(this Entity scene, FUIViewId viewId, object userdata, ETCancellationToken cancellationToken)
+        {
+            return await scene.GetComponent<FUIComponent>().OpenUIAsync(viewId, userdata, cancellationToken);
+        }
+
+        public static ETTask<FUI> OpenUIAsync(this Entity scene, FUIViewId viewId, object userdata)
+        {
+            return scene.OpenUIAsync(viewId, userdata, default);
+        }
+
+        public static ETTask<FUI> OpenUIAsync(this Entity scene, FUIViewId viewId, ETCancellationToken cancellationToken)
+        {
+            return scene.OpenUIAsync(viewId, null, cancellationToken);
+        }
+        
+        public static ETTask<FUI> OpenUIAsync(this Entity scene, FUIViewId viewId)
+        {
+            return scene.OpenUIAsync(viewId, null, default);
+        }
+    }
+}

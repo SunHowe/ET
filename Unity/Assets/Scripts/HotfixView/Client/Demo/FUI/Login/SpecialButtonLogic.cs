773540692d78fd39751daf65a315e8d1c61959c7
SunHowe <tosunhowe@gmail.com>
2024-07-23 11:30:32 +0800

    [chore] demo ui

--- Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Login/SpecialButtonLogic.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Login/SpecialButtonLogic.cs
@@ -1,18 +1,27 @@
-﻿namespace ET.Client.Login
+﻿using FairyGUI;
+
+namespace ET.Client.Login
 {
     [FUICompLogic(typeof(SpecialButton))]
     public partial class SpecialButtonLogic : AFUICompLogic<SpecialButton>
     {
         protected override void OnInitialize() 
         {
+            this.ContentPane.onAddedToStage.Add(OnAddedToStage);
         }
 
         protected override void OnDispose() 
         {
+            this.ContentPane.onAddedToStage.Remove(OnAddedToStage);
         }
 
         protected override void OnSetupAfterAdd() 
         {
+        }
+
+        private void OnAddedToStage(EventContext context)
+        {
+            Log.Info("SpecialButtonLogic OnAddedToStage");
         }
     }
 

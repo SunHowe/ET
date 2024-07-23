d8836317043cc17ab8f43e1aa942a2faae84a978
SunHowe <tosunhowe@gmail.com>
2024-07-23 10:07:02 +0800

    [chore] 移除UGUI组件和系统

--- Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Login/AppStartInitFinish_CreateLoginForm.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Login/AppStartInitFinish_CreateLoginForm.cs
@@ -1,7 +1,7 @@
 ﻿namespace ET.Client
 {
 	[Event(SceneType.Demo)]
-	public class AppStartInitFinish_CreateLoginUI: AEvent<Scene, AppStartInitFinish>
+	public class AppStartInitFinish_CreateLoginForm: AEvent<Scene, AppStartInitFinish>
 	{
 		protected override async ETTask Run(Scene root, AppStartInitFinish args)
 		{
